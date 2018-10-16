b2d7606cf206639f43f3185b0decf174f82ce6df
mt <mt@mts-Mac.local>
2018-10-16 11:19:50 +1100

    first

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # scrounger-json2report
 Parses json output from the perform_full_analysis module to readable output
+
+Run `npm link` to link the bin file to your system path.
---
+++ index.js
@@ -0,0 +1,29 @@
+const minimist = require('minimist')
+const fs = require('fs');
+
+module.exports = () => {
+	const args = minimist(process.argv.slice(2))
+	const cmd = args._[0]
+
+	if (!fs.existsSync(cmd)) {
+		console.log('Usage: '+process.argv[1]+' <scrounger-results-json>');
+		if (cmd != null) {
+			console.log('File does not exist: '+cmd);
+		}
+		process.exit(1);
+	}
+
+	var contents = fs.readFileSync(cmd);
+	var json = JSON.parse(contents);
+
+	json.forEach(function(entry) {
+		console.log('--- '+entry.severity+' - '+entry.title);
+
+		entry.details = entry.details.replace(/\n \* L/g, '\n  * L');
+		if (entry.details == '') {
+			entry.details = 'No evidence, thanks for that Scrounger';
+		}
+
+		console.log(entry.details.trim()+'\n');
+	});
+}
